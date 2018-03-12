5abc8af541c2191e3fd4a55adcdd5b42f6f024c9
Iñaki Cabo Alonso <icaboalo@gmail.com>
2018-03-11 21:26:09 -0600

    Update Team.cs

--- Fifa/Team.cs
+++ Fifa/Team.cs
@@ -25,7 +25,7 @@ namespace Fifa
             this.players = new List<Player>();
             SqlConnection con = Connection.addConnection();
             con.Open();
-            SqlCommand cmd = new SqlCommand(String.Format("SELECT * FROM jugador INNER JOIN equipo ON jugador.idEquipo = equipo.id WHERE equipo.nombre = '{0}';", this.name), con);
+            SqlCommand cmd = new SqlCommand(String.Format("SELECT jugador.* FROM jugador INNER JOIN equipo ON jugador.idEquipo = equipo.id WHERE equipo.nombre = '{0}';", this.name), con);
             SqlDataReader reader = cmd.ExecuteReader();
             while (reader.Read()) {
                 players.Add(new Player(reader.GetInt16(0), reader.GetString(1), reader.GetString(2), reader.GetInt16(3)));
